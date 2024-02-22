793debf469e6fc8ff6a036e3fe5bc68e0958f1a3
flapperz <flap.atyz@gmail.com>
2024-02-22 20:20:11 +0700

    Refactor + new display

--- python/main.py
+++ python/main.py
@@ -1,59 +1,123 @@
 import datetime
 import calendar
+import pandas as pd
+import argparse
 
 log_fname = "/Users/flap/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/work-timer-log.txt"
 
-read_data = {
+
+weekday_abbr_map = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+
+TODAY_BANNER = \
+'''-----
+TODAY
+-----
+'''
+
+def read_data(fname):
+    raw_data = {
     '0' : [],
     '1' : []
-}
-
-parse_data = []
-
-def main():
-    with open(log_fname, 'r') as file:
+    }
+    
+    with open(fname, 'r') as file:
         for line in file:
             op, timestr = line.strip().split("|")
             x = datetime.datetime.strptime(timestr, "%Y-%m-%dT%H:%M:%S%z")
+            raw_data[op].append(x)
             
-            read_data[op].append(x)
-    
-    N = len(read_data['0'])       
-    if N == 0:
-        return 
-         
-    # parse read_data
+    return raw_data
+        
 
+def parse_data(data):
+    parse_data = dict()
+    for start_t, stop_t in zip(data['1'], data['0']):
+            
+            duration = stop_t - start_t
+            year = start_t.year
+            month = start_t.month
+            day = start_t.day
+            weekday = start_t.weekday()
+            
+            # parse_data.append(((year, month, day), weekday, duration))
+            key = (year, month, day)
+            if key not in parse_data:
+                parse_data[key] = {
+                    'weekday'   : weekday,
+                    'durations' : [],
+                    'startdts'  : []
+                }
+            parse_data[key]['durations'].append(duration)
+            parse_data[key]['startdts'].append(start_t)
+    return parse_data
+
+def format_timedelta(td):
+    return ':'.join([f"{int(s):02}" for s in str(td).split(':')[:2]])
+
+def generate_report(data, today_ymd, is_verbose):
     
-    for start_t, stop_t in zip(read_data['1'], read_data['0']):
-        
-        duration = stop_t - start_t
-        year = start_t.year
-        month = start_t.month
-        day = start_t.day
-        weekday = start_t.weekday()
-        
-        parse_data.append(((year, month, day), weekday, duration))
+    outstr = ""
     
-    curr_year = None
-    curr_month = None
-    curr_day = None
-    for ((year, month, day), weekday, duration) in parse_data:
-        if year != curr_year:
-            curr_year = year
-            print(f"{year}:")
-        if month != curr_month:
-            curr_month = month
-            month_name = calendar.month_name[month]
-            print(f"  {month_name}:")
-            curr_month == month
-        if day != curr_day:
-            curr_day = day
-            weekday_name = calendar.day_name[weekday]
-            print(f"    {weekday_name} - {day}:")
-        print(f"      {duration}")
+    
+    for ymd, v in data.items():
+        
+        if not is_verbose and ymd != today_ymd:
+            continue
+            
+        y,m,d = ymd
+        weekday = v['weekday']
+        weekday_abbr = weekday_abbr_map[weekday]
+        durations = v['durations']
+        sum_durations = sum(durations, datetime.timedelta())
+        sum_durations_format = format_timedelta(sum_durations)
+        
+        dayheader = f"{weekday_abbr} {d:02}-{m:02}-{y:04}:"
+        
+        if ymd != today_ymd:
+            outstr += f"{dayheader} {sum_durations_format}\n"
+        else:
+            outstr += TODAY_BANNER
+            outstr += f"{dayheader} {sum_durations_format} - sum\n"
+                
+            for i, duration in enumerate(durations):
+                duration_format = format_timedelta(duration)
+                outstr += f"{' '*len(dayheader)} {duration_format} - session {i+1}\n"
+            
+            
+    outstr = outstr.strip()
+    return outstr
+    
+
+
+def get_today_ymd():
+    date_time_now = datetime.datetime.now()
+    today_y = date_time_now.year
+    today_m = date_time_now.month
+    today_d = date_time_now.day
+    # print(date_time_now.timetuple())
+    
+    return (today_y, today_m, today_d) 
+
+
+def main(args):
+    
+    is_verbose = args.verbose
+    
+    raw_data = read_data(log_fname)
+    
+    day_entries_map = parse_data(raw_data)
+    
+    today_ymd = get_today_ymd()
+    
+    report = generate_report(day_entries_map, today_ymd, is_verbose)
+    
+    print(report)
             
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(prog='Work Timer Analysis')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    args = parser.parse_args()
+    
+    main(args)
